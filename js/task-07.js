a7fce80b48b2983a1f747d0c249cf59e6fda6236
Snovanakid <denchapro@gmail.com>
2023-03-15 16:12:02 +0200

    Update task-7 (rangeInput.value)

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,6 @@
 const rangeInput = document.querySelector('#font-size-control');
-const spanText = document.querySelector('#text')
-spanText.style.fontSize = '50px'
+const spanText = document.querySelector('#text');
+spanText.style.fontSize = `${rangeInput.value}px`; 
 
 rangeInput.addEventListener('input', (evt) => {
     spanText.style.fontSize = `${evt.target.value}px`
